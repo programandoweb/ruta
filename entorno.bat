c7e95b4f4e4b8d8defe43e73b605bd9aa693b686
Jorge Méndez <lic.jorgemendez@gmail.com>
2025-09-10 23:23:31 -0500

    Update

--- entorno.bat
+++ entorno.bat
@@ -1,11 +1,11 @@
 D:
-start cmd /K "cd D:\lexor.cloud\backend.lexor.cloud"
+start cmd /K "cd D:\lexor.cloud\backend.linkcargologistics.com"
 D:
-#start cmd /K "cd D:\lexor.cloud\backend.lexor.cloud && php artisan serve --host=0.0.0.0 --port=8000"
-start cmd /K "cd D:\lexor.cloud\backend.lexor.cloud && php artisan serve"
+#start cmd /K "cd D:\lexor.cloud\backend.linkcargologistics.com && php artisan serve --host=0.0.0.0 --port=8000"
+start cmd /K "cd D:\lexor.cloud\backend.linkcargologistics.com && php artisan serve"
 D:
-start cmd /K "cd D:\lexor.cloud\dashboard.lexor.cloud && npm run dev"
+start cmd /K "cd D:\lexor.cloud\dashboard.linkcargologistics.com && npm run dev"
 D:
-start cmd /K "cd D:\lexor.cloud\dashboard.lexor.cloud"
+start cmd /K "cd D:\lexor.cloud\dashboard.linkcargologistics.com"
 
 
